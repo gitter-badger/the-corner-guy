cfa67da3f8dd7ea4f26095bf4530eaae3ac1f614
shian48263 <shian48263@gmail.com>
2016-02-17 21:24:15 +0800

    added filter parseHtml to rendeer html string

---
+++ client/app/parseHtml/parseHtml.filter.spec.js
@@ -0,0 +1,19 @@
+'use strict';
+
+describe('Filter: parseHtml', function () {
+
+  // load the filter's module
+  beforeEach(module('theCornerGuyApp'));
+
+  // initialize a new instance of the filter before each test
+  var parseHtml;
+  beforeEach(inject(function ($filter) {
+    parseHtml = $filter('parseHtml');
+  }));
+
+  it('should return the input prefixed with "parseHtml filter:"', function () {
+    var text = 'angularjs';
+    expect(parseHtml(text)).to.equal('parseHtml filter: ' + text);
+  });
+
+});
